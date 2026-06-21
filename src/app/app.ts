a3365fe807a1a70741238ea6ac6438a779670e7c
Shilratna Chawhan <shilratnachawhan@Shilratnas-MacBook-Air.local>
2026-06-21 11:08:44 +0530

    updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,11 +9,49 @@ import { Common } from './services/common';
 })
 export class App {
   protected readonly title = signal('Angular-UI');
- alertMessage: { type: string; text: string } | null = null;
+  alertMessage: { type: string; text: string } | null = null;
 
   constructor(private alertService: Common) {
     this.alertService.alertMessage$.subscribe((message) => {
       this.alertMessage = message;
     });
+  }
+
+  private scriptsLoaded = false;
+
+  async ngOnInit() {
+    if (!this.scriptsLoaded) {
+      await this.loadScripts();
+      this.scriptsLoaded = true;
+    }
+  }
+
+  loadScripts() {
+    const scriptUrls = [
+      'assets/js/bootstrap.bundle.min.js',
+      'assets/js/sidebar-menu.js',
+      'assets/js/quill.min.js',
+      'assets/js/data-table.js',
+      'assets/js/prism.js',
+      'assets/js/clipboard.min.js',
+      'assets/js/simplebar.min.js',
+      'assets/js/apexcharts.min.js',
+      'assets/js/echarts.min.js',
+      'assets/js/swiper-bundle.min.js',
+      'assets/js/fullcalendar.main.js',
+      'assets/js/jsvectormap.min.js',
+      'assets/js/world-merc.js',
+      'assets/js/custom/apexcharts.js',
+      'assets/js/custom/echarts.js',
+      'assets/js/custom/maps.js',
+      'assets/js/custom/custom.js'
+    ];
+
+    for (const url of scriptUrls) {
+      const script = document.createElement('script');
+      script.src = url;
+      script.async = true;
+      document.body.appendChild(script);
+    }
   }
 }
--- src/app/modules/shared/components/layout/layout.ts
+++ src/app/modules/shared/components/layout/layout.ts
@@ -7,39 +7,4 @@ import { Component } from '@angular/core';
   styleUrl: './layout.css',
 })
 export class Layout {
-  ngOnInit(): void {
-    this.loadScripts();
-  }
-
-  async loadScripts() {
-    const scriptUrls = [
-      'assets/js/bootstrap.bundle.min.js',
-      'assets/js/sidebar-menu.js',
-      'assets/js/quill.min.js',
-      'assets/js/data-table.js',
-      'assets/js/prism.js',
-      'assets/js/clipboard.min.js',
-      'assets/js/simplebar.min.js',
-      'assets/js/apexcharts.min.js',
-      'assets/js/echarts.min.js',
-      'assets/js/swiper-bundle.min.js',
-      'assets/js/fullcalendar.main.js',
-      'assets/js/jsvectormap.min.js',
-      'assets/js/world-merc.js',
-      'assets/js/custom/apexcharts.js',
-      'assets/js/custom/echarts.js',
-      'assets/js/custom/maps.js',
-      'assets/js/custom/custom.js'
-    ];
-
-    for (const url of scriptUrls) {
-      await new Promise<void>((resolve, reject) => {
-        const script = document.createElement('script');
-        script.src = url;
-        script.onload = () => resolve();
-        script.onerror = () => reject();
-        document.body.appendChild(script);
-      });
-    }
-  }
 }
